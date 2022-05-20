593d48f0ab309feb5228d0c693f0acf48200e32a
Aline T <43286541+aline-tg@users.noreply.github.com>
2022-05-19 22:12:20 -0300

    Added utils module

---
+++ br.com.cataemprego/control/utils.js
@@ -0,0 +1,17 @@
+export function createButton(inputId, buttonClass, text) {
+    /**
+     * Creates a button element
+     * 
+     * @param inputId the button id
+     * @param buttonClass the css class used to style button
+     * @param text the button text to be seen in UI
+     * 
+     * @return button a button element
+     */
+    var button = document.createElement("button");
+    button.classList.add(buttonClass);
+    button.innerText = text;
+    button.id = inputId;
+
+    return button;
+}
