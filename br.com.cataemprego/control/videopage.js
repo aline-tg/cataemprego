0bfef75c884037bb9da717a40b1c31903f4779c3
Aline T <43286541+aline-tg@users.noreply.github.com>
2022-05-25 21:04:25 -0300

    Added images

--- br.com.cataemprego/control/videopage.js
+++ br.com.cataemprego/control/videopage.js
@@ -1,18 +1,7 @@
-import {createButton} from "./utils.js";
-
 export function userPerson() {
-   window.location.href = "./view/pages/videopage.html"; 
-   var myDiv = document.createElement("div");
-   myDiv.setAttribute('style', 'text-align:center;');
-
-   var button1 = createButton("videopage_back","video_page_btn", "Voltar");
-   var button2 = createButton("videopage_next","video_page_btn", "Próximo");
-   myDiv.appendChild(button1);
-   myDiv.appendChild(button2);
-
-   document.body.appendChild(myDiv);
+   window.location.href = "./view/pages/videopage_user.html"; 
 }  
 
 export function userCompany() {
-    window.location.href = "./view/pages/videopage.html"; 
+    window.location.href = "./view/pages/videopage_company.html"; 
  }
