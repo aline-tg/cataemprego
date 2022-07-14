006aae900bcdf520d87323f42039dda1c4935b3f
Aline T <43286541+aline-tg@users.noreply.github.com>
2022-07-13 22:57:30 -0300

    Updated UI in search feature

--- control/searchSupabase.js
+++ control/searchSupabase.js
@@ -23,35 +23,27 @@ class Position {
   }
 }
 
-export default async function searchJobs(search,
-                                         messageToPrint, 
+export default async function searchJobs(search, 
                                          supabaseClient) {   
 
     var listJobsFound = [];
-    let message = messageToPrint;
     const {data: all_jobs, error: error_reading} = await supabaseClient
     .from("registered-jobs")
     .select("*")
     .contains("position_keywords",[search])
 
-    console.log(all_jobs)
-
-    // all_jobs.forEach(position_filtered => {
-    //   // var position = new Position(position_filtered.id,
-    //   //                             position_filtered.company_id,
-    //   //                             position_filtered.company_name,
-    //   //                             position_filtered.position_name,
-    //   //                             position_filtered.position_location,
-    //   //                             position_filtered.position_duty_hours,
-    //   //                             position_filtered.position_salary,
-    //   //                             position_filtered.position_benefits,
-    //   //                             position_filtered.position_description)
-    //   // listJobsFound.push(position)
-    //   console.log(position_filtered)
-    // })
-
-    if (listJobsFound.length == 0)
-      message = "Não foram encontradas vagas para a posição buscada."
-    else
-      message = "Foram encontradas " + listJobsFound.length + "vagas."
-}
+    all_jobs.forEach(position_filtered => {
+      var position = new Position(position_filtered.id,
+                                  position_filtered.company_id,
+                                  position_filtered.company_name,
+                                  position_filtered.position_name,
+                                  position_filtered.position_location,
+                                  position_filtered.position_duty_hours,
+                                  position_filtered.position_salary,
+                                  position_filtered.position_benefits,
+                                  position_filtered.position_description)
+      listJobsFound.push(position)
+    })
+    
+    return listJobsFound
+  }
