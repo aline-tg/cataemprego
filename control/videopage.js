a99ae30bf215f2630a1a51a8cf173342ef5c6c50
Aline T <43286541+aline-tg@users.noreply.github.com>
2022-08-14 14:44:10 -0300

    Added space

--- control/videopage.js
+++ control/videopage.js
@@ -3,6 +3,7 @@ export function userPerson() {
    window.location.href = "./view/pages/videopage_user.html"; 
 }  
 
+
 export function userCompany() {
     window.location.href = "./view/pages/videopage_company.html"; 
  }
